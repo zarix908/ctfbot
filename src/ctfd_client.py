1e53fcff8a08310968de45cb41734633b4368f63
zarix908 <daniyarrus@gmail.com>
2022-09-27 01:57:49 +0500

    finish ctfd client

--- src/ctfd_client.py
+++ src/ctfd_client.py
@@ -1,3 +1,4 @@
+import base64
 from uuid import uuid4
 
 import requests
@@ -8,7 +9,9 @@ from models.user import User
 
 def register_users(users):
     user: User = users.get()
-    json_user = {
+    print(f'[CTFd client] register user {user}...')
+
+    reg_data = {
         'name': user.tg_username,
         'email': f'{user.tg_username}@example.com',  # TODO add email
         'password': str(uuid4()),
@@ -16,15 +19,28 @@ def register_users(users):
         'verified': False,
         'hidden': False,
         'banned': False,
-        'fields': [
-            {'field_id': 1, 'value': user.last_name},
-            {'field_id': 2, 'value': user.first_name},
-            {'field_id': 4, 'value': user.course}
-        ]
+        # 'fields': [
+        #     {'field_id': 1, 'value': user.last_name},
+        #     {'field_id': 2, 'value': user.first_name},
+        #     {'field_id': 4, 'value': user.course}
+        # ]
     }
-    r = requests.post(
-        f'{config.ctfd_url}/api/v1/users',
-        headers={'Authorization': f'Token {config.ctfd_token}'},
-        json=json_user
-    )
-    print(r.ok)
+    req = {
+        'telegram_id': user.tg_id,
+        'reg_data': reg_data
+    }
+
+    try:
+        r = requests.post(
+            f'{config.ctfd_url}/telegram/register',
+            headers={'Authorization': f'Token {config.ctfd_token}'},
+            json=req
+        )
+        if not r.ok:
+            print(
+                f'[CTFd client] add user failed, code: {r.status_code}, response: {base64.b64encode(r.content)}'
+            )
+            return
+        print(f'[CTFd client] user added')
+    except Exception as e:
+        print(f'[CTFd client] add user failed: {e}')
